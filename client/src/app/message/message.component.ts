317a2e16702aee6e17a5a927c7b2409eeee73e45
M.Vlasova <m.vlasova@niias.ru>
2019-02-01 15:56:28 +0300

    add mesage and user list components

---
+++ client/src/app/message/message.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-body',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent {
+
+  constructor(){
+
+  }
+
+}
